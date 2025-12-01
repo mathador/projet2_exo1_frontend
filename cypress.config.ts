f4d3ba9ed3d2109602546d93ea816d103ed3eb3a
Chris <chrismachu@gmail.com>
2025-12-01 12:11:14 +0100

    tests login

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  env: {
+    baseUrl: "http://localhost:4200",
+    apiUrl: "http://localhost:8080/api",
+    //authToken: "your-auth-token-here"
+    login_url: '/login',
+    logout_api: '/logout'
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/pages/login.cy.js
@@ -0,0 +1,82 @@
+/// <reference types="cypress" />
+
+context('login page', () => {
+
+    before(() => {
+        cy.request({
+            method: 'GET',
+            url: Cypress.env('apiUrl') + Cypress.env('logout_api'),
+            failOnStatusCode: false
+        })
+        .then((response) => {
+            if (response.status !== 404) {
+                cy.log("✅ web api ok")
+            } else {
+                cy.log(("❌ web api url not found: " + Cypress.env('apiUrl') + Cypress.env('logout_api')))
+            }
+        });
+    })
+
+    beforeEach(() => {
+        //cy.log(Cypress.env('baseUrl'))
+        cy.visit(Cypress.env('baseUrl') + Cypress.env('login_url'))
+        // cy.request("POST", apiUrl + "/login", {
+        //     "username": fakeEmail,
+        //     "password": fakePassword
+        // }).then((response) => {
+        //     token = response.body.token;
+        //     // Stockez le token dans la variable
+        // });
+
+    })
+
+    it('check input controls, errors labels and buttons', () => {
+        cy.title().should('include', 'EtudiantFrontend')
+        cy.getByDataCy('login').should('exist')
+        cy.getByDataCy('password').should('exist')
+        cy.getByDataCy('btn-valider').should('exist')
+        cy.getByDataCy('btn-annuler').should('exist')
+    })
+
+    it('click valider without any field fullfilled', () => {
+        cy.getByDataCy('btn-valider').click()
+        cy.getByDataCy('login').should('have.class', 'is-invalid')
+        cy.getByDataCy('password').should('have.class', 'is-invalid')
+    })
+
+    it('click valider without password field fullfilled', () => {
+        cy.getByDataCy('login').type('MyLogin')
+        cy.getByDataCy('btn-valider').click()
+        cy.getByDataCy('password').should('have.class', 'is-invalid')
+    })
+
+    it('click valider without login field fullfilled', () => {
+        cy.getByDataCy('password').type('MyPassword')
+        cy.getByDataCy('btn-valider').click()
+        cy.getByDataCy('login').should('have.class', 'is-invalid')
+    })
+
+    it('click cancel clear all fields', () => {
+        cy.getByDataCy('login').type('MyLogin')
+        cy.getByDataCy('password').type('MyPassword')
+        cy.getByDataCy('btn-annuler').click()
+        cy.getByDataCy('login').should('have.value', '')
+        cy.getByDataCy('password').should('have.value', '')
+    })
+    
+    it('try to log with existing user but wrong password', () => {
+        cy.getByDataCy('login').type('d')
+        cy.getByDataCy('password').type('MyPassword')
+        cy.getByDataCy('btn-valider').click()
+        // TODO problème à résoudre avec l'api ou frontend
+        cy.getByDataCy('btn-register').should('exist')
+    })    
+
+    
+    it('try to log with existing user and good password', () => {
+        cy.getByDataCy('login').type('d')
+        cy.getByDataCy('password').type('d')
+        cy.getByDataCy('btn-valider').click()
+        cy.getByDataCy('btn-logout').should('exist') 
+    })   
+})
