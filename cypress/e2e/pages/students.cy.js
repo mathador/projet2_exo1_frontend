88b21755db37c31d627b8a8105eb46f660d7911d
Chris <chrismachu@gmail.com>
2025-12-02 12:02:01 +0100

    quelques ajouts de tests students

--- cypress/e2e/pages/students.cy.js
+++ cypress/e2e/pages/students.cy.js
@@ -14,32 +14,67 @@ context('students page', () => {
         cy.getByDataCy('students-table').should('exist')
     })
 
-    it('try to remove student', () => {
+    it('try to remove student, and confirm', () => {
 
         cy.get('table tr')
             .last()
             .find('td')
             .first()
             .invoke('text')
-            .then(t => { 
-                const idStudent = t.trim(); 
-                cy.log('ID of student to delete: ' + idStudent);         
-                
+            .then(t => {
+                const idStudent = t.trim();
+                cy.log('ID of student to delete: ' + idStudent);
+
                 cy.get('table tr')
-                .last()
-                .within(() => {
-                    cy.contains('button', 'Supprimer').click();
-                });
-                
+                    .last()
+                    .within(() => {
+                        cy.contains('button', 'Supprimer').click();
+                    });
+
                 cy.on('window:confirm', () => true);
-                
+
                 cy.get('table tr')
-                .last()
-                .find('td')
-                .first()
-                .invoke('text')
-                .should('not.equal', idStudent);
+                    .last()
+                    .find('td')
+                    .first()
+                    .invoke('text')
+                    .should('not.equal', idStudent);
             });
     });
 
+    it('try to remove student but finally cancel', () => {
+
+        cy.get('table tr')
+            .last()
+            .find('td')
+            .first()
+            .invoke('text')
+            .then(t => {
+                const idStudent = t.trim();
+                cy.log('ID of student to delete: ' + idStudent);
+
+                cy.get('table tr')
+                    .last()
+                    .within(() => {
+                        cy.contains('button', 'Supprimer').click();
+                    });
+
+                cy.on('window:confirm', () => false);
+
+                cy.get('table tr')
+                    .last()
+                    .find('td')
+                    .first()
+                    .invoke('text')
+                    .should('equal', idStudent);
+            });
+    });
+
+    it('create new student', () => {
+        cy.getByDataCy('btn-create-student').click()
+        cy.url()
+            .should('contain', 'student')
+            .and('not.contain', 'students')
+            // ici on ne va pas au bout de la création car celà serait redondant avec les tests du fichier student.cy.js
+    })
 })
